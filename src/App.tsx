71742497379abb51451f7bafac71fde9b292fd3d
EShanbaum <shanbaumethan@gmail.com>
2025-06-13 12:45:34 -0400

    Fixed styling for title and CSVUpload

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import { AnalysisPanel } from "./components/AnalysisPanel";
 
 function App() {
   return (
-    <div className="p-4 space-y-4">
-      <h1 className="text-2xl font-bold">Data Analysis Tool</h1>
-      <CSVUpload />
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-center">Data Analysis Tool</h1>          
+      <div className="justify-center flex mb-4">
+        <CSVUpload />
+      </div>
       <DataTable />
       <GraphBuilder />
       <AnalysisPanel />
